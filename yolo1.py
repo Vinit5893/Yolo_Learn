f5e695e60fbc414f01182015da3cb1b454d960d4
Vinit Kumar <vinitkumar5893@gmail.com>
2023-08-09 19:19:40 +0530

    update code

--- yolo1.py
+++ yolo1.py
@@ -3,10 +3,19 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-classNames = 
+classesFile = 'coco.names'
+classNames = []
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+print(classNames)
+print(len(classNames))
 
 while True:
     success, img = cap.read()
+
     cv2.imshow('Image',img)
-    cv2.waitKey(1)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+      break
     
+
