e66134018bdade2a662dbde2107ed9c7675a9ad1
Vinit Kumar <vinitkumar5893@gmail.com>
2023-08-09 22:28:00 +0530

    u1

--- yolo1.py
+++ yolo1.py
@@ -2,20 +2,38 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+whT = 320
 
 classesFile = 'coco.names'
 classNames = []
 with open(classesFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
     
-print(classNames)
-print(len(classNames))
+# print(classNames)
+# print(len(classNames))
+
+modelConfiguration = 'yolov3.cfg'
+modelWeights = 'yolov3.weights'
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration,modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
 while True:
     success, img = cap.read()
 
-    cv2.imshow('Image',img)
+    blob = cv2.dnn.blobFromImage(img,1/255,(whT,whT),[0,0,0],1,crop=False)
+    net.setInput(blob)
 
+    layerNames = net.getLayerNames()
+    # print(layerNames)
+    outputNames = [layerNames[(0)-1] for i in net.getUnconnectedOutLayers()]
+    print(outputNames)
+    print(net.getUnconnectedOutLayers())
+
+
+    cv2.imshow('Image',img)
+    # cv2.waitKey(1)
     if cv2.waitKey(1) & 0xFF == 27:
       break
     
