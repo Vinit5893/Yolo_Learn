e966629e45eef7facf4e41b35ff8c949395e6bce
Vinit Kumar <vinitkumar5893@gmail.com>
2023-08-10 18:34:54 +0530

    Output Verified

--- yolo1.py
+++ yolo1.py
@@ -3,6 +3,8 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 whT = 320
+confThreshold = 0.5
+nmsThreshold = 0.3
 
 classesFile = 'coco.names'
 classNames = []
@@ -12,12 +14,44 @@ with open(classesFile, 'rt') as f:
 # print(classNames)
 # print(len(classNames))
 
-modelConfiguration = 'yolov3.cfg'
-modelWeights = 'yolov3.weights'
+modelConfiguration = 'yolov3-320.cfg'
+modelWeights = 'yolov3-320.weights'
 
 net = cv2.dnn.readNetFromDarknet(modelConfiguration,modelWeights)
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+def findObjects(outputs,img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w,h = int(det[2]*wT), int(det[3]*hT)
+                x,y = int((det[0]*wT)-w/2), int((det[1]*hT)-h/2)
+                bbox.append([x,y,w,h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+    # print(len(bbox))
+    indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+    print(indices)
+    for i in indices:
+        # i = i[0]
+        box = bbox[i]
+        x,y,w,h = box[0], box[1], box[2], box[3]
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,255), 3)
+        cv2.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i]*100)}%', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,255),2)
+
+
+
+
+
 
 while True:
     success, img = cap.read()
@@ -31,13 +65,13 @@ while True:
     # print(outputNames)
     # print(net.getUnconnectedOutLayers())
 
-    output = net.forward(outputNames)
-    print(output[0].shape)
-    print(output[1].shape)
-    print(output[2].shape)
-    print(output[0][0])
+    outputs = net.forward(outputNames)
+    # print(outputs[0].shape)
+    # print(outputs[1].shape)
+    # print(outputs[2].shape)
+    # print(outputs[0][0])
 
-
+    findObjects(outputs,img)
 
     cv2.imshow('Image',img)
     # cv2.waitKey(1)
